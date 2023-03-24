72139756d29f926b0691f1e9b9af9613caecfd05
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-03-24 18:04:27 +0000

    add more linux commands

--- duo.sh
+++ duo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 
 while true; do
     read -p $'\e[32muser@mint-\e[0m\e[32mmint.lol:\e[0m\e[34m~\e[0m\e[32m$ \e[0m' command
@@ -18,19 +19,91 @@ while true; do
         exit)
             break
             ;;
-        ls)
-            ls
+        ls*)
+            ls "${command#ls }"
             ;;
-        cd)
-            read -p "Enter the directory path: " dir_path
-            cd "$dir_path"
+        cd*)
+            cd "${command#cd }"
             ;;
         pwd)
             pwd
             ;;
+        mint)
+            mint
+            ;;
+        touch*)
+            touch "${command#touch }"
+            ;;
+        mkdir*)
+            mkdir "${command#mkdir }"
+            ;;
+        rm*)
+            rm "${command#rm }"
+            ;;
+        cat*)
+            cat "${command#cat }"
+            ;;
         echo*)
             echo "${command#echo }"
             ;;
+        grep*)
+            grep "${command#grep }"
+            ;;
+        ps*)
+            ps "${command#ps }"
+            ;;
+        top*)
+            top "${command#top }"
+            ;;
+        cp*)
+            cp "${command#cp }"
+            ;;
+        mv*)
+            mv "${command#mv }"
+            ;;
+        chmod*)
+            chmod "${command#chmod }"
+            ;;
+        chown*)
+            chown "${command#chown }"
+            ;;
+        tar*)
+            tar "${command#tar }"
+            ;;
+        find*)
+            find "${command#find }"
+            ;;
+        ssh*)
+            ssh "${command#ssh }"
+            ;;
+        scp*)
+            scp "${command#scp }"
+            ;;
+        rsync*)
+            rsync "${command#rsync }"
+            ;;
+        unzip*)
+            unzip "${command#unzip}"
+            ;;
+        zip*)
+            zip "${command#zip }"
+            ;;
+        base64*)
+            base64 "${command#base64 }"
+            ;;
+
+        history)
+            history
+            ;;
+
+        jobs)
+            jobs
+            ;;
+
+        open*)
+            xdg-open "${command#open }"
+            ;;
+
         *)
             echo "Invalid command: $command"
             ;;
@@ -38,4 +111,3 @@ while true; do
 done
 
 echo "Exited Duo terminal."
-

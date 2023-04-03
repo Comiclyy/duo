3992c15164dad819506161eea093b2bdc77f370e
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-04-03 18:52:48 +0000

    duo

--- installer/duo-install.py
+++ installer/duo-install.py
@@ -12,5 +12,10 @@ os.chmod(filename, 0o755)
 # Move file to /usr/local/bin
 os.system(f"sudo mv {filename} /usr/local/bin")
 
+configurl = "https://raw.githubusercontent.com/Comicly69/duo/main/.duo/config.json"
+
+os.mkdir(".duo")
+urllib.request.urlretrieve(configurl, ".duo/config.json")
+
 # Print message to reload terminal
 print("Reload terminal to use duo.")
